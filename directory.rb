9e57a6ec468d733f871eb31a559d494a475f6d37
Jack Oddy <jackoddy91@gmail.com>
2016-06-20 19:57:22 +0100

    stores student data in an array of hashes using symbols as keys to make methods more descriptive.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,17 @@
 #names of all students in array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"]
+{ name: "Dr. Hannibal Lecter", cohort: :november},
+{ name: "Darth Vader", cohort: :november},
+{ name: "Nurse Ratched", cohort: :november},
+{ name: "Michael Corleone", cohort: :november},
+{ name: "Alex DeLarge", cohort: :november},
+{ name: "The Wicked Witch of the West", cohort: :november},
+{ name: "Terminator", cohort: :november},
+{ name: "Freddy Krueger", cohort: :november},
+{ name: "The Joker", cohort: :november},
+{ name: "Joffrey Baratheon", cohort: :november},
+{ name: "Norman Bates", cohort: :november}
+]
 
 def print_header   #method to print out header
   puts "The students of Villains Academy"
@@ -18,7 +19,7 @@ def print_header   #method to print out header
 end
 
 def print_names roster   #lists all the name of the students
-  roster.each { |name| puts name }
+  roster.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer (names)  #will show how many there are enrolled
