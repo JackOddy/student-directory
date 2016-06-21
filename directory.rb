60c49abd721aa84566b24b8540cab797b8e888ad
Jack Oddy <jackoddy91@gmail.com>
2016-06-21 11:22:09 +0100

    Adds numbers to student list when it prints out according to position in Students Array

--- directory.rb
+++ directory.rb
@@ -6,7 +6,7 @@ students = []
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   name = gets.chomp
-  
+
   #recursively loops while name has a value to allow multiple entry
   while !name.empty? do
     #as .chomp removes the last enter, using it twice breaks loop as it will be nil
@@ -25,7 +25,7 @@ def print_header   #method to print out header
 end
 
 def print_names roster   #lists all the name of the students
-  roster.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  roster.each.with_index(1) { |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer (names)  #will show how many there are enrolled
